80fc26e587c5d2b44a9ccd4aea7c2fe07f0c4a8f
paulvaldez <valdezpaul00@gmail.com>
2017-10-16 11:54:56 -0500

    Completed the Deployment and Dependencies exercise.

--- src/main/java/com/codeup/App.java
+++ src/main/java/com/codeup/App.java
@@ -1,6 +1,7 @@
 package com.codeup;
 
 import org.apache.commons.lang3.StringUtils;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -8,9 +9,27 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
         boolean isBlank = StringUtils.isBlank("");
         System.out.println(isBlank);
+
+        System.out.println("Enter something:");
+        String alpha = scanner.nextLine();
+
+
+        if (StringUtils.isNumeric(alpha)) {
+            System.out.println(alpha + " is a number");
+        } else {
+            System.out.println(alpha + " is not a number");
+        }
+        System.out.println("Flipped case: " + StringUtils.swapCase(alpha));
+        System.out.println("Reversed: " + StringUtils.reverse(alpha));
+
+
     }
 }
+
